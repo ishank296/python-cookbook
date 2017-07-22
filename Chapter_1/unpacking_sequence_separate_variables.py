aa5ec4f7bc328a24c409bd92065af97cf23b4d58
ishank <ishank296@gmail.com>
2017-07-23 01:00:17 +0530

    fist commit

---
+++ Chapter_1/unpacking_sequence_separate_variables.py
@@ -0,0 +1,19 @@
+p=(4,5)
+x,y=p
+print(x)
+print(y)
+
+data = ['ACME',50,91.1,(2017,12)]
+name,share,price,date=data
+print (name)
+print (share) 
+print (price)
+print (date)
+
+s = 'Hello'
+a,b,c,d,e=s
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
