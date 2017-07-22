36ef63bba732ca259e803398be10185002c33e7d
ishank <ishank296@gmail.com>
2017-07-23 01:30:12 +0530

    commit 2

---
+++ Chapter_1/unpacking_sequence_arbitrary_variables.py
@@ -0,0 +1,16 @@
+
+#problem : to get average of all elements excep first and last
+
+def drop_first_last(lis_grades):
+    first,*middle,last = lis_grades
+    return middle
+
+print(drop_first_last([1,2,5,7,8,4,9]))
+
+
+record = ('Dave','dave@example.com','777-234-983','565-322-098')
+name,email,*phone=record
+print(name)
+print(email)
+print(phone)
+
