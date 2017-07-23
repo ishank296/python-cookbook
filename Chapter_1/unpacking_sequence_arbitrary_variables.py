f0c112e3d7eb417d291f82d6baa911a4887293f2
ishank <ishank296@gmail.com>
2017-07-24 01:16:39 +0530

    commmit 3

--- Chapter_1/unpacking_sequence_arbitrary_variables.py
+++ Chapter_1/unpacking_sequence_arbitrary_variables.py
@@ -14,3 +14,48 @@ print(name)
 print(email)
 print(phone)
 
+
+*trailing, current = [10, 8, 7, 1, 9, 5, 10, 3]
+print(trailing)
+print(current)
+
+
+records=[
+('foo',1,2),
+('bar','hello'),
+('foo',45,46)
+]
+
+def do_foo(x,y):
+	print('foo',x,y)
+	
+def do_bar(s):
+	print('bar',s)
+	
+
+for tag,*args in records:
+	if tag=='foo':
+		do_foo(*args)
+	elif tag=='bar':
+		do_bar(*args)
+		
+		
+line = 'nobody:*-2:-2:Unprivileged User:/var/tmp/:/user/bin'.split(':')
+uname,*fields,homedir,sh = line
+print(uname)
+print(fields)
+print(sh)
+
+record = ('ACME',50,123.45,(12.18,2012))
+name,*_,(*_,year) = record
+print(name)
+print(year)
+
+def sum(items):
+	head,*tail= items
+	return head + sum(tail) if tail else head
+	
+print(sum([4,6,9,5,3,2]))
+		
+		
+		
