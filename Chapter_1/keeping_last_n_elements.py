dd57cbf39245bf4071e7402bc6afe271df77426e
ishank <ishank296@gmail.com>
2017-07-26 23:47:04 +0530

    deque api examples

--- Chapter_1/keeping_last_n_elements.py
+++ Chapter_1/keeping_last_n_elements.py
@@ -8,6 +8,30 @@ def search(lines,pattern,history=5):
 		previous_lines.append(line)
 		
 		
+
+"""Using deque(maxlen=N) creates a fixed-sized queue. When new items are added and the queue is full, the oldest item is automatically removed"""
+q = deque(maxlen=3)
+q.append(3)
+q.append(1)
+q.append(2)
+print(q)
+
+q.append(4)
+q.append(5)
+print(q)
+q.appendleft(7)
+print(q)	
+q.popleft()
+print(q)	
+q.pop()
+print(q)
+q.extend('werwe')
+print(q)	
+q.rotate(1)
+print(q)
+q.rotate(2)
+print(q)	
+		
 if __name__=='__main__':
 	with open('test.txt') as f:
 		for line,previous_lines in search(f,' is ',5):
